d7442526a587c945504f57194b43f625aa5c12ae
ChauGiaHuy <chaugiahuy02102000@gmail.com>
2021-02-18 12:44:25 +0700

    Added website

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Auth;
+
+Route::group(['prefix' => 'panel', 'namespace' => 'admin'], function() {	
+	Route::get('login','LoginController@getLogin')->name('getLogin');
+	Route::post('login','LoginController@postLogin')->name('postLogin');
+	Route::get('logout','LoginController@getLogout')->name('getLogout');
+});
+
+Route::group(['middleware' => 'CheckAdminLogin','prefix' => 'panel'], function() {
+	Route::get('/', function() {return view('admin.home');})->name('welcome');
+});
+
+Route::group(['middleware' => 'CheckAdminLogin','prefix' => 'panel/user', 'namespace' => 'admin'], function() {
+	Route::get('/', 'adminUserController@index')->name('user.index');
+	Route::get('index','UserController@index')->name('user.index');
+	Route::get('add','UserController@getadd')->name('user.getadd');
+	Route::post('add','UserController@postadd')->name('user.postadd');
+	Route::get('edit/{id}','UserController@getedit')->name('user.getedit');
+	Route::post('edit/{id}','UserController@postedit')->name('user.postedit');
+	Route::get('delete/{id}','UserController@delete')->name('user.delete');
+});
